1fbc69718f010f6c5e80e5c5d12ef69a2f51ad8b
alejandro3469 <alejandro.perez3469@gmail.com>
2024-02-15 22:50:36 -0600

    icons

--- app/menu/List.tsx
+++ app/menu/List.tsx
@@ -4,11 +4,20 @@ import styles from "@/app/menu/list.module.css";
 import { Open_Sans } from "next/font/google";
 export const roboto_slab = Open_Sans({ subsets: ["latin"], display: "swap" });
 
+import { FaShrimp } from "react-icons/fa6";
+
 export default function List() {
   return (
     <div className={`${styles.container} ${roboto_slab.className}`}>
       <span className={styles.decoration} />
-      <h2 className={styles.title}>Mariscos</h2>
+
+      <h2 className={styles.title}>
+        <span className={styles.icon_title}>
+          <FaShrimp />
+          Mariscos
+        </span>{" "}
+        <span className={styles.items}>{"(4 platillos)"}</span>
+      </h2>
       <ul>
         <li>
           <Dish

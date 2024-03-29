3701373a9a5ec5a132143d41b5486f10964ab1da
alejandro3469 <alejandro.perez3469@gmail.com>
2024-03-28 22:41:48 -0600

    styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { DM_Serif_Display } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
 import Link from "next/link";
 import { fetchDishes } from "@/app/lib/data";
+import { FaArrowRight } from "react-icons/fa";
+import { BsArrowUpRight } from "react-icons/bs";
 
 import { Roboto } from "next/font/google";
 
@@ -15,10 +17,18 @@ export default async function Home() {
   const dishes = await fetchDishes();
   return (
     <main>
-      <Link href="/menu/simple">Menu</Link>
-      <Link href="/edit-dishes">Edit Dishes</Link>
-      <Link href="/create-items">Create Items</Link>
-      <Link href="/delete-items">Delete Items</Link>
+      <Link className="dashboard-link" href="/menu/simple">
+        Menu <BsArrowUpRight />
+      </Link>
+      <Link className="dashboard-link" href="/edit-dishes">
+        Edit Dishes <BsArrowUpRight />
+      </Link>
+      <Link className="dashboard-link" href="/create-items">
+        Create Items <BsArrowUpRight />
+      </Link>
+      <Link className="dashboard-link" href="/delete-items">
+        Delete Items <BsArrowUpRight />
+      </Link>
     </main>
   );
 }

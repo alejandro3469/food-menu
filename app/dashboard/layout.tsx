cbd70dffd0f32d4c219d46321d5f98a61bd10aba
alejandro3469 <alejandro.perez3469@gmail.com>
2024-03-31 21:42:08 -0600

    nav styles

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { DM_Serif_Display } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
@@ -14,18 +16,20 @@ import { Roboto } from "next/font/google";
 import { fetchDishes, fetchCategories } from "@/app/lib/data";
 import { PiNotebook } from "react-icons/pi";
 
+import { usePathname } from "next/navigation";
+
 const roboto = Roboto({ weight: ["100", "300", "400"], subsets: ["latin"] });
 
 const dmserifdisplay = DM_Serif_Display({ weight: "400", subsets: ["latin"] });
 const playfairdisplay = Playfair_Display({ weight: "400", subsets: ["latin"] });
 
-export default async function Dashboard({
+export default function Dashboard({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  const dishes = await fetchDishes();
-  const categories = await fetchCategories();
+  const pathname = usePathname();
+
   return (
     <main className={`${roboto.className}`}>
       <div className={styles.mobile_navbar}>
@@ -39,25 +43,41 @@ export default async function Dashboard({
             <span>{categories.length}</span>
           </div>{" "}
         </div>*/}
-        <Link className={styles.dashboard_link} href="/menu/simple">
+
+        <Link className={`${styles.dashboard_link}`} href="/menu/simple">
           <span className={styles.menu_icon}>
             <PiNotebook />
           </span>
           <span className={styles.text}>Menu</span>
         </Link>
-        <Link className={styles.dashboard_link} href="/dashboard/">
+        <Link
+          className={`${
+            pathname.toString() != "/dashboard"
+              ? styles.dashboard_link
+              : styles.active
+          }`}
+          href="/dashboard/"
+        >
           <span className={styles.menu_icon}>
             <LiaEdit />
           </span>
           <span className={styles.text}>Edit</span>
         </Link>
-        <Link className={styles.dashboard_link} href="/dashboard/create-items">
+        <Link className={`${
+            pathname.toString() != "/dashboard/create-items"
+              ? styles.dashboard_link
+              : styles.active
+          }`} href="/dashboard/create-items">
           <span className={styles.menu_icon}>
             <IoAddSharp />
           </span>
           <span className={styles.text}>Create</span>
         </Link>
-        <Link className={styles.dashboard_link} href="/dashboard/delete-items">
+        <Link  className={`${
+            pathname.toString() != "/dashboard/delete-items"
+              ? styles.dashboard_link
+              : styles.active
+          }`} href="/dashboard/delete-items">
           <span className={styles.menu_icon}>
             <PiTrashSimpleLight />
           </span>

d3335a0d27bfe169538326140e20a17bf8f0a700
alejandro3469 <alejandro.perez3469@gmail.com>
2024-04-02 23:29:58 -0600

    search bar

--- app/dashboard/edit-dishes/page.tsx
+++ app/dashboard/edit-dishes/page.tsx
@@ -1,8 +1,21 @@
 import Form from "@/app/ui/edit-form";
 import { fetchDishes, fetchCategories } from "@/app/lib/data";
 import styles from "@/app/ui/form.module.css";
+import { Suspense } from 'react';
+import Search from "@/app/ui/search";
 
-export default async function Page() {
+
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
   const dishes = await fetchDishes();
   const categories = await fetchCategories();
@@ -10,8 +23,11 @@ export default async function Page() {
     <>
       <h1 className={styles.title}>Editar/Modificar</h1>
 
+      <Search placeholder="Search invoices..." />
+
       <div className={styles.container}>
         {dishes.map((dish, index) => (
+          
           <div key={index}>
             <Form dish={dish} categories={categories} />
           </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,50 @@
 import Form from "@/app/ui/edit-form";
 import { fetchDishes, fetchCategories } from "@/app/lib/data";
 import styles from "@/app/ui/form.module.css";
-import { useState } from "react";
+import { Suspense } from "react";
+import Search from "@/app/ui/search";
 
-export default async function Page() {
-  //const [search, setSearch] = useState("");
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
 
   const dishes = await fetchDishes();
   const categories = await fetchCategories();
   return (
-    <main>
+    <>
       <h1 className={styles.title}>Editar/Modificar</h1>
 
+      {query}
+
+      <Search placeholder="Search invoices..." />
+
       <div className={styles.container}>
-        {dishes.map((dish, index) => (
-          <div key={index}>
-            <Form dish={dish} categories={categories} />
-          </div>
-        ))}
+        {query != "" &&
+          dishes.map(
+            (dish, index) =>
+              dish.name.toLowerCase().indexOf(query.toLowerCase()) != -1 && (
+                <div key={index}>
+                  <Form dish={dish} categories={categories} />
+                  {dish.name}
+                  {dish.name.indexOf(query)}
+                </div>
+              )
+          )}
+        {query == "" &&
+          dishes.map((dish, index) => (
+            <div key={index}>
+              <Form dish={dish} categories={categories} />
+              {dish.name}
+            </div>
+          ))}
       </div>
-    </main>
+    </>
   );
 }

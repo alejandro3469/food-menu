684febb36898408577e608d5d3865b09b287f618
alejandro3469 <alejandro.perez3469@gmail.com>
2024-04-03 23:13:07 -0600

    Update Footer.tsx

--- app/menu/Footer.tsx
+++ app/menu/Footer.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { FaRegCopyright } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="footer">
       <span className="copyright">
         <FaRegCopyright />
-        Cocina La Antigua
+        2024 Cocina La Antigua
       </span>
+      <div>
+        <h3>Ponte en contacto con nosotros</h3>
+        <ul>
+          <li>55 0000 0000</li>
+          <li>emailexample@domain.com</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Ubicacion</h3>
+        <Link
+          className="maps_location"
+          href={"https://maps.app.goo.gl/zVqB5uGK3w6hRzBe9"}
+        >
+          La antigua, Tultepec, Estado de Mexico 
+        </Link>
+      </div>
+      
     </div>
   );
 }

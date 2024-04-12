2c7251786ed240cf8be125b9a9c917f5d2c2d56c
alejandro3469 <alejandro.perez3469@gmail.com>
2024-04-12 11:57:28 -0600

    vercel analytics

--- app/menu/simple/page.tsx
+++ app/menu/simple/page.tsx
@@ -6,6 +6,7 @@ import { Dishes, Categories } from "@/app/lib/definitions";
 import { FiExternalLink } from "react-icons/fi";
 import { VscLinkExternal } from "react-icons/vsc";
 import Menu2 from "../Menu2";
+import { Analytics } from "@vercel/analytics/react"
 
 const nunito = Roboto({ weight: "300", subsets: ["latin"] });
 const playfairdisplay = Playfair_Display({
@@ -19,6 +20,7 @@ import { Kalnia } from "next/font/google";
 import Link from "next/link";
 import MenuPage2 from "../MenuPage2";
 import Footer from "../Footer";
+
 const kalnia = Kalnia({
   weight: ["400", "500", "600"],
   subsets: ["latin"],
@@ -31,6 +33,8 @@ export default function Home() {
         <Menu2 mode={0} />
       </MenuPage2>
       <Footer />
+      <Analytics/>
+      
     </main>
   );
 }
